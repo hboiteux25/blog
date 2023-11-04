5ee1755a94622ed9e77b6f7f55a0c81122e8ba4c
Henrique Boiteux <henriqueboiteux62@gmail.com>
2023-11-03 22:12:04 -0300

    create menu

---
+++ src/components/Article/index.js
@@ -0,0 +1,12 @@
+import Post from '../Post'
+
+function Article(){
+    return(
+        <>
+            <h1>Articles</h1>
+            <Post> </Post>
+        </>
+        
+    );
+}
+export default Article;
