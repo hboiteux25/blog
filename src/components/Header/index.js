c7991f125a221525c53369d3754f3eca1385d35b
Henrique Boiteux <henriqueboiteux62@gmail.com>
2023-11-03 23:03:52 -0300

    create menu update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,27 @@
+import logosvg from '../../svg/blogger-logo.png'
+
 function Header(){
+
+    const descLogo = "Essa é logo do blog"
+
     return(
-        <h1>Header.js</h1>
+        <>
+            <header className="flex-space-between">
+                <div className="Logo border">
+                    <img src={logosvg} alt="{descLogo}" width="50px"></img>
+                </div>
+                <div className="search">
+                    <input type='text' name="search" className='input-search'/>
+                </div>
+                <div className="menu">
+                    <ul className='menu'>
+                        <li><a href='#' className='nav-link'>Category</a></li>
+                        <li><a href='#' className='nav-link'>About</a></li>
+                        <li><a href='#' className='nav-link'>Contact</a></li>
+                    </ul>
+                </div>
+            </header>
+        </>
     );
 }
 
